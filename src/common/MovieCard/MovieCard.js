b833d2779ac964130e1b8ac6c65029feaefa80d2
jihyun1029 <parkji2028@naver.com>
2025-04-17 11:36:07 +0900

    장르 보여주기

--- src/common/MovieCard/MovieCard.js
+++ src/common/MovieCard/MovieCard.js
@@ -4,8 +4,16 @@ import './MovieCard.style.css'
 import {useMovieGenreQuery} from "../../hooks/useMovieGenre";
 
 const MovieCard = ({movie}) => {
-    const {data:genreData} = useMovieGenreQuery();
-    console.log("ggg", genreData);
+    const {data: genreData} = useMovieGenreQuery();
+
+    const showGenre = (genreIdList) => {
+        if (!genreData) return []
+        const genreNameList = genreIdList.map((id) => {
+            const genreObj = genreData.find((genre) => genre.id === id)
+            return genreObj.name;
+        });
+        return genreNameList
+    }
 
     return <div
         style={{
@@ -18,13 +26,13 @@ const MovieCard = ({movie}) => {
     >
         <div className="overlay">
             <h1>{movie.title}</h1>
-            {movie.genre_ids.map((genre, index) => (
+            {showGenre(movie.genre_ids).map((genre, index) => (
                 <Badge bg="danger" key={index} className="me-1">{genre}</Badge>
             ))}
             <div>
                 <div>{movie.vote_average}</div>
                 <div>{movie.popularity}</div>
-                <div>{movie.adult?'over18':'under18'}</div>
+                <div>{movie.adult ? 'over18' : 'under18'}</div>
             </div>
         </div>
     </div>
